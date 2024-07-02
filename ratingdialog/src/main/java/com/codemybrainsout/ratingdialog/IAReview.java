c00d9db098eae2ff51372ab3ab88d8a3623f04db
hieunx <hieunx>
2024-07-02 21:33:06 +0700

    update rate

--- ratingdialog/src/main/java/com/codemybrainsout/ratingdialog/IAReview.java
+++ ratingdialog/src/main/java/com/codemybrainsout/ratingdialog/IAReview.java
@@ -24,7 +24,7 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.tasks.Task;
+import com.google.android.gms.tasks.Task;
 
 public class IAReview {
     private static volatile IAReview INSTANCE;
